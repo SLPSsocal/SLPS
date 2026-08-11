f3ccf54bd7b48bd1bb271287ea0092826dad82c5
SLPSsocal <krishannagin@gmail.com>
2026-08-10 22:49:35 -0700

    Add files via upload

--- netlify/functions/members.js
+++ netlify/functions/members.js
@@ -2,7 +2,7 @@ const https = require("https");
 
 const BASE_ID = "appts3pknGCDwe6bk";
 const TABLE_ID = "tblvbEMVk7zr24B2c";
-const VIEW_ID  = "viwu7lk33kGWPFYYs";
+const VIEW_ID  = "viwNrRzG9funeJggp";
 
 function fetchPage(token, offset) {
   return new Promise((resolve, reject) => {
